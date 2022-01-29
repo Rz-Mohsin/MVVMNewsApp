d8e960f8da500119390644e9057843d5d8c42a13
MOHSIN <mohsinarifpcc@gmail.com>
2022-01-29 10:52:38 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants  {
+    companion object{
+        const val API_KEY = "a3ccef384d33400e9463218e20bd20b6"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
